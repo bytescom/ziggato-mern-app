85975c238c155baf3a62c602cde25352eb5ea254
bytescom <pankajk07414@gmail.com>
2026-05-17 02:12:25 +0530

    feat: upgrading sweet shop app to food delivery web app and add more features, functionality.

--- backend/middleware/adminValidate.js
+++
@@ -1,9 +0,0 @@
-const adminOnly = (req, res, next) => {
-    if (req.user && req.user.role === "admin") {
-        next();
-    } else {
-        return res.status(403).json({ message: "Admin access only" });
-    }
-};
-
-export default adminOnly;
